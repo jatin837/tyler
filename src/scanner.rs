316701dce6e00587a3f179ba7a8d88e7df9b9172
dj4t9n <pandeyjatin47@gmail.com>
2021-06-22 18:11:38 +0530

    comment out java like syntax

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,9 +2,9 @@ struct Scanner {
     source: String,
     tokens: Vec<Token> = Vec::new;
   
-    Scanner(String source) {
-      this.source = source;
-    }
+    // Scanner(String source) {
+    //   .source = source;
+    // }
 }
 
 impl Scanner {
