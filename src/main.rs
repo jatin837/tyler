e383f1d5bb403f31aa246fcf25752c69d53b7c8a
dj4t9n <pandeyjatin47@gmail.com>
2021-06-22 15:34:15 +0530

    STATUS:STUCK DESCRIPTION: =========== 	how to i split the content of file. 	does there exist a function in rust. 	or should i create my own?

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,63 @@ use path_abs::PathAbs;
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
-
 //use std::io::Read;
+
+mod token_type {
+    enum token_type {
+        // Single-character tokens.
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        COMMA,
+        DOT,
+        MINUS,
+        PLUS,
+        SEMICOLON,
+        SLASH,
+        STAR,
+
+        // One or two character tokens.
+        BANG,
+        BANG_EQUAL,
+        EQUAL,
+        EQUAL_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL,
+
+        // Literals.
+        IDENTIFIER,
+        STRING,
+        NUMBER,
+
+        // Keywords.
+        AND,
+        CLASS,
+        ELSE,
+        FALSE,
+        FUN,
+        FOR,
+        IF,
+        NIL,
+        OR,
+        PRINT,
+        RETURN,
+        SUPER,
+        THIS,
+        TRUE,
+        VAR,
+        WHILE,
+
+        EOF,
+    }
+}
+
+fn tokenize(raw: &String) -> Vec<String>{
+    vec!{String::from("ab"), String::from("dev")}
+}
 
 fn run_prompt(){
     // TOBE implemented
