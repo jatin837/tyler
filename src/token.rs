b1b6e7ae56a248290c0c92b30f9cd360c97929f6
dj4t9n <pandeyjatin47@gmail.com>
2021-06-22 17:50:23 +0530

    damn man rust is over complicated, i need to under how to structure code

--- src/token.rs
+++ src/token.rs
@@ -1,21 +1,9 @@
-mod libs;
-libs::*;
 pub struct Token {
     TYPE: TokenType,
     lexeme: String,
     literal: Literal,
     line: i32, 
   
-    // Token(TokenType type, String lexeme, Object literal, int line) {
-    //   this.type = type;
-    //   this.lexeme = lexeme;
-    //   this.literal = literal;
-    //   this.line = line;
-    // }
-  
-    // public String toString() {
-    //   return type + " " + lexeme + " " + literal;
-    // }
   }
 pub impl Token {
       to_string(&self){
