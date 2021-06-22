d3f7b94d80e5f8399eba437055bed31314207d91
dj4t9n <pandeyjatin47@gmail.com>
2021-06-22 15:56:03 +0530

    unable to include libs into file tree

--- src/libs.rs
+++ src/libs.rs
@@ -1,4 +1,11 @@
-enum token_type {
+use std::env;
+use std::path::Path;
+use path_abs::PathAbs;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+pub enum token_type {
     // Single-character tokens.
     LEFT_PAREN,
     RIGHT_PAREN,
@@ -46,4 +53,37 @@ enum token_type {
     WHILE,
 
     EOF,
+}
+
+//use std::io::Read;
+
+
+
+pub fn tokenize(raw: &String) -> Vec<String>{
+    vec!{String::from("ab"), String::from("dev")}
+}
+
+pub fn run_prompt(){
+    // TOBE implemented
+    loop {
+        let mut line = String::new();
+        println!("\x1b[0;31mtyli>>\x1b[0m");
+        let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        println!("no of bytes read , {}", b1);
+    }
+}
+
+pub fn to_bytes(raw_text: &String) -> &[u8] {
+    raw_text.as_bytes()
+}
+
+pub fn run_file(fpath: &Path) {
+//reading file first
+    let fp = File::open(&fpath).expect("[ERROR] opening file");
+    let mut buf_reader = BufReader::new(fp);
+    let mut src_code = String::new();
+    buf_reader.read_to_string(&mut src_code).expect("[ERROR] can't read file");
+    let bytes = to_bytes(&src_code);
+    println!("File {:?} \n-----------\n{:?}", fpath, src_code);
+    println!("bytes string  {:?} \n-----------\n{:?}", fpath, bytes);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,11 @@
 use std::env;
 use std::path::Path;
 use path_abs::PathAbs;
-use std::io::prelude::*;
-use std::fs::File;
-use std::io::BufReader;
-//use std::io::Read;
-use tyler::token_type
-
-
-
-fn tokenize(raw: &String) -> Vec<String>{
-    vec!{String::from("ab"), String::from("dev")}
-}
-
-fn run_prompt(){
-    // TOBE implemented
-    loop {
-        let mut line = String::new();
-        println!("\x1b[0;31mtyli>>\x1b[0m");
-        let b1 = std::io::stdin().read_line(&mut line).unwrap();
-        println!("no of bytes read , {}", b1);
-    }
-}
-
-fn to_bytes(raw_text: &String) -> &[u8] {
-    raw_text.as_bytes()
-}
-
-fn run_file(fpath: &Path) {
-//reading file first
-    let fp = File::open(&fpath).expect("[ERROR] opening file");
-    let mut buf_reader = BufReader::new(fp);
-    let mut src_code = String::new();
-    buf_reader.read_to_string(&mut src_code).expect("[ERROR] can't read file");
-    let bytes = to_bytes(&src_code);
-    println!("File {:?} \n-----------\n{:?}", fpath, src_code);
-    println!("bytes string  {:?} \n-----------\n{:?}", fpath, bytes);
-}
+use tyler::{
+    to_bytes,
+    run_file,
+    run_prompt,
+};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
