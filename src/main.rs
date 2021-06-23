96ca05af75aa96794af58f7bb37bb86a571bf31f
dj4t9n <pandeyjatin47@gmail.com>
2021-06-23 17:46:11 +0530

    wrap into mod

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,112 @@
 use path_abs::PathAbs;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::path::Path;
-
 //use std::io::Read;
 
-enum TokenType {
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
-    COMMA,
-    DOT,
-    MINUS,
-    PLUS,
-    SEMICOLON,
-    SLASH,
-    STAR,
+pub mod helper {
+    use std::io::prelude::*;
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::path::Path;
+    pub fn run_prompt() {
+        // TOBE implemented
+        loop {
+            let mut line = String::new();
+            println!("\x1b[0;31mtyli>>\x1b[0m");
+            let b1 = std::io::stdin().read_line(&mut line).unwrap();
+            println!("no of bytes read , {}", b1);
+        }
+    }
 
-    // One or two character tokens.
-    BANG,
-    BANG_EQUAL,
-    EQUAL,
-    EQUAL_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    LESS,
-    LESS_EQUAL,
+    pub fn to_bytes(raw_text: &String) -> &[u8] {
+        raw_text.as_bytes()
+    }
 
-    // Literals.
-    IDENTIFIER,
-    STRING,
-    NUMBER,
-
-    // Keywords.
-    AND,
-    CLASS,
-    ELSE,
-    FALSE,
-    FUN,
-    FOR,
-    IF,
-    NIL,
-    OR,
-    PRINT,
-    RETURN,
-    SUPER,
-    THIS,
-    TRUE,
-    VAR,
-    WHILE,
-
-    EOF,
-}
-
-struct Scanner {
-    token_list : Vec<Token>,
-}
-
-struct Token {
-    Type: TokenType,
-    line: i64,
-    literal: String,
-    lexeme: String,
-}
-
-fn run_prompt() {
-    // TOBE implemented
-    loop {
-        let mut line = String::new();
-        println!("\x1b[0;31mtyli>>\x1b[0m");
-        let b1 = std::io::stdin().read_line(&mut line).unwrap();
-        println!("no of bytes read , {}", b1);
+    pub fn run_file(fpath: &Path) {
+        //reading file first
+        let fp = File::open(&fpath).expect("[ERROR] opening file");
+        let mut buf_reader = BufReader::new(fp);
+        let mut src_code = String::new();
+        buf_reader
+            .read_to_string(&mut src_code)
+            .expect("[ERROR] can't read file");
+        let bytes = to_bytes(&src_code);
+        print!("File {:?} \n-----------\n{:?}", fpath, src_code);
+        print!("bytes string  {:?} \n-----------\n{:?}", fpath, bytes);
     }
 }
 
-fn to_bytes(raw_text: &String) -> &[u8] {
-    raw_text.as_bytes()
+pub mod scanner {
+    use crate::token::Token;
+
+    struct Scanner {
+        token_list : Vec<Token>,
+    }
 }
 
-fn run_file(fpath: &Path) {
-    //reading file first
-    let fp = File::open(&fpath).expect("[ERROR] opening file");
-    let mut buf_reader = BufReader::new(fp);
-    let mut src_code = String::new();
-    buf_reader
-        .read_to_string(&mut src_code)
-        .expect("[ERROR] can't read file");
-    let bytes = to_bytes(&src_code);
-    print!("File {:?} \n-----------\n{:?}", fpath, src_code);
-    print!("bytes string  {:?} \n-----------\n{:?}", fpath, bytes);
+pub mod token {
+    pub enum TokenType {
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        COMMA,
+        DOT,
+        MINUS,
+        PLUS,
+        SEMICOLON,
+        SLASH,
+        STAR,
+
+        // One or two character tokens.
+        BANG,
+        BANG_EQUAL,
+        EQUAL,
+        EQUAL_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL,
+
+        // Literals.
+        IDENTIFIER,
+        STRING,
+        NUMBER,
+
+        // Keywords.
+        AND,
+        CLASS,
+        ELSE,
+        FALSE,
+        FUN,
+        FOR,
+        IF,
+        NIL,
+        OR,
+        PRINT,
+        RETURN,
+        SUPER,
+        THIS,
+        TRUE,
+        VAR,
+        WHILE,
+
+        EOF,
+    }  
+    pub struct Token {
+        Type: TokenType,
+        line: i64,
+        literal: String,
+        lexeme: String,
+    }
 }
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        1 => run_prompt(),
+        1 => helper::run_prompt(),
         2 => {
             let filename: &String = &args[1];
             let abs_file_path = PathAbs::new(filename).unwrap();
@@ -105,7 +114,7 @@ fn main() {
             if !filepath.exists() {
                 eprintln!("[ERROR] provide a file that exists")
             } else {
-                run_file(&filepath)
+                helper::run_file(&filepath)
             }
         }
         _ => eprintln!("[USAGE] tyler <file_name> (for script), tyler (for running prompt)"),
